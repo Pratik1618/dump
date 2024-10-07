503a2647d9b58ed4af991042ed6f5086afad82df
Pratik1618 <77444747+Pratik1618@users.noreply.github.com>
2024-10-08 01:21:14 +0530

    Delete vowel.py

--- vowel.py
+++
@@ -1,7 +0,0 @@
-def vow(v):
-    if v in ['a', 'e', 'i', 'o', 'u']:
-         print("The alphabet is a vowel")
-    else:
-        print("The alphabet is a consonant")
-alpha = input("Enter your alphabet: ")
-vow(alpha.lower())
